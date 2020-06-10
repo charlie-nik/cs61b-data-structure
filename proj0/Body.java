a054846b5c747acc0578b31530c00e6841160422
Alice Qian Huang <alice.huangqian97@gmail.com>
2020-06-09 19:12:37 -0500

    changed variable G from public to private

--- proj0/Body.java
+++ proj0/Body.java
@@ -8,7 +8,7 @@ public class Body {
 	public String imgFileName;
 
 	/** The gravitational constant G.*/
-	public static final double G = 6.67e-11
+	static final double G = 6.67e-11
 	;
 
 	public Body(double xP, double yP, double xV,
