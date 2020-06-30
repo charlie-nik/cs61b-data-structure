f9a01876c8b2c763ec3cfaf1f1d7e6fe641c5c64
Alice Qian Huang <alice.huangqian97@gmail.com>
2020-06-30 13:48:07 -0500

    Compared runtime: BFS is a bit faster than DFS (because of my input data?)

--- clab9/EnemyTest.java
+++ clab9/EnemyTest.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
+
+import edu.princeton.cs.algs4.Stopwatch;
 import org.junit.Test;
 
 import java.io.FileNotFoundException;
@@ -7,27 +9,80 @@ import java.io.FileNotFoundException;
 public class EnemyTest {
 
     @Test
-    public void twoGroups() throws FileNotFoundException {
-        int N = 2;
-        assertFalse(basic1(N).isSeparable());
-        assertTrue(basic2(N).isSeparable());
-        assertFalse(basic3(N).isSeparable());
-        assertTrue(party1(N).isSeparable());
-        assertTrue(party2(N).isSeparable());
-        assertFalse(party3(N).isSeparable());
-        assertFalse(party4(N).isSeparable());
+    public void DFS() throws FileNotFoundException {
+        twoGroupsDFS();
+        threeGroupsDFS();
     }
 
     @Test
-    public void threeGroups() throws FileNotFoundException {
+    public void BFS() throws FileNotFoundException {
+        twoGroupsBFS();
+        threeGroupsBFS();
+    }
+
+    @Test
+    public void compareDFSandBFS() throws FileNotFoundException {
+        int N = 100;
+
+        Stopwatch sw = new Stopwatch();
+        for (int i = 0; i < N; i++) {
+            DFS();
+        }
+        System.out.println("DFS: " + sw.elapsedTime() + " seconds.");
+
+        sw = new Stopwatch();
+        for (int i = 0; i < N; i++) {
+            BFS();
+        }
+        System.out.println("BFS: " + sw.elapsedTime() + " seconds.");
+    }
+
+    @Test
+    public void twoGroupsDFS() throws FileNotFoundException {
+        int N = 2;
+        assertFalse(basic1(N).isSeparableDFS());
+        assertTrue(basic2(N).isSeparableDFS());
+        assertFalse(basic3(N).isSeparableDFS());
+        assertTrue(party1(N).isSeparableDFS());
+        assertTrue(party2(N).isSeparableDFS());
+        assertFalse(party3(N).isSeparableDFS());
+        assertFalse(party4(N).isSeparableDFS());
+    }
+
+    @Test
+    public void threeGroupsDFS() throws FileNotFoundException {
         int N = 3;
-        assertTrue(basic1(N).isSeparable());
-        assertTrue(basic2(N).isSeparable());
-        assertTrue(basic3(N).isSeparable());
-        assertTrue(party1(N).isSeparable());
-        assertTrue(party2(N).isSeparable());
-        assertTrue(party3(N).isSeparable());
-        assertTrue(party4(N).isSeparable());
+        assertTrue(basic1(N).isSeparableDFS());
+        assertTrue(basic2(N).isSeparableDFS());
+        assertTrue(basic3(N).isSeparableDFS());
+        assertTrue(party1(N).isSeparableDFS());
+        assertTrue(party2(N).isSeparableDFS());
+        assertTrue(party3(N).isSeparableDFS());
+        assertTrue(party4(N).isSeparableDFS());
+    }
+
+    @Test
+    public void twoGroupsBFS() throws FileNotFoundException {
+        int N = 2;
+        assertFalse(basic1(N).isSeparableDFS());
+        assertTrue(basic2(N).isSeparableDFS());
+        assertFalse(basic3(N).isSeparableDFS());
+        assertTrue(party1(N).isSeparableDFS());
+        assertTrue(party2(N).isSeparableDFS());
+        assertFalse(party3(N).isSeparableDFS());
+        assertFalse(party4(N).isSeparableDFS());
+    }
+
+    @Test
+    public void threeGroupsBFS() throws FileNotFoundException {
+        int N = 3;
+        assertTrue(basic1(N).isSeparableDFS());
+        assertTrue(basic2(N).isSeparableDFS());
+        assertTrue(basic3(N).isSeparableDFS());
+        assertTrue(party1(N).isSeparableDFS());
+        assertTrue(party2(N).isSeparableDFS());
+        assertTrue(party3(N).isSeparableDFS());
+        assertTrue(party4(N).isSeparableDFS());
     }
 
     /* TEST UTILITY. */
