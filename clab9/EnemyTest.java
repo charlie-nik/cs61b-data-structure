0bbc90c64ad45ff0c7fc0a1e19d796faa9ab0319
Alice Qian Huang <alice.huangqian97@gmail.com>
2020-06-30 13:11:59 -0500

    Further cleanup (feels good)

--- clab9/EnemyTest.java
+++ clab9/EnemyTest.java
@@ -3,9 +3,6 @@ import static org.junit.Assert.assertFalse;
 import org.junit.Test;
 
 import java.io.FileNotFoundException;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 public class EnemyTest {
 
@@ -31,54 +28,6 @@ public class EnemyTest {
         assertTrue(party2(N).isSeparable());
         assertTrue(party3(N).isSeparable());
         assertTrue(party4(N).isSeparable());
-    }
-
-    /* TEST ENEMY CLASS. */
-
-    @Test
-    public void printEnemies() throws FileNotFoundException {
-        Enemy e = new Enemy(party3(3).g, 2);
-        printEnemies(e);
-    }
-
-    @Test
-    public void printTables() throws FileNotFoundException {
-        Enemy e = new Enemy(party1(2).g, 2);
-        printTables(e);
-    }
-
-    @Test
-    public void printAll() throws FileNotFoundException {
-        Enemy e = new Enemy(basic2(2).g, 2);
-        printEnemies(e);
-        System.out.println();
-        printTables(e);
-        System.out.println();
-        System.out.println("Separable: " + e.isSeparable);
-    }
-
-    private void printEnemies(Enemy e) {
-        Map<String, Set<String>> enemyOf = e.getEnemyOf();
-        System.out.println("Enemy list:");
-        for (String p : enemyOf.keySet()) {
-            System.out.print(p + ": ");
-            for (String s : enemyOf.get(p)) {
-                System.out.print(s + ", ");
-            }
-            System.out.println();
-        }
-    }
-
-    private void printTables(Enemy e) {
-        List<Set<String>> forbidden = e.getForbidden();
-        System.out.println("Tables:");
-        for (int i = 0; i < forbidden.size(); i++) {
-            System.out.print("Table " + (i + 1) + " forbidden members: ");
-            for (String s : forbidden.get(i)) {
-                System.out.print(s + ", ");
-            }
-            System.out.println();
-        }
     }
 
     /* TEST UTILITY. */
