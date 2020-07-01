5b0c20920089e80eeef55ec6570238957031e983
Alice Qian Huang <alice.huangqian97@gmail.com>
2020-07-01 18:43:55 -0500

    Implemented cycle-detector in maze using DFS

--- lab10/lab10/graphs/MazeCycles.java
+++ lab10/lab10/graphs/MazeCycles.java
@@ -9,6 +9,8 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int[] tempEdgeTo;
+    private boolean cycleFound = false;
 
     public MazeCycles(Maze m) {
         super(m);
@@ -16,8 +18,33 @@ public class MazeCycles extends MazeExplorer {
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        tempEdgeTo = new int[maze.V()];
+        dfs(0);
+    }
+    
+    private void dfs(int v) {
+        while (!cycleFound) {
+            marked[v] = true;
+            announce();
+            for (int w : maze.adj(v)) {
+                if (marked[w] && w != tempEdgeTo[v]) {
+                    cycleFound = true;
+                    drawCycle(v, w);
+                    return;
+                } else if (!marked[w]) {
+                    tempEdgeTo[w] = v;
+                    dfs(w);
+                }
+            }
+        }
     }
 
-    // Helper methods go here
+    private void drawCycle(int v, int w) {
+        edgeTo[w] = v;
+        while (v != w) {
+            edgeTo[v] = tempEdgeTo[v];
+            v = tempEdgeTo[v];
+        }
+        announce();
+    }
 }
