19b94a94370a53bdccb24d603fc5870bc5126a2f
Alice Qian Huang <alice.huangqian97@gmail.com>
2020-07-01 18:39:05 -0500

    Reset Lab 10 to skeleton to track my work

--- lab10/lab10/graphs/MazeAStarPath.java
+++ lab10/lab10/graphs/MazeAStarPath.java
@@ -1,9 +1,5 @@
 package lab10.graphs;
 
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.PriorityQueue;
-
 /**
  *  @author Josh Hug
  */
@@ -22,15 +18,9 @@ public class MazeAStarPath extends MazeExplorer {
         edgeTo[s] = s;
     }
 
-    /** Estimate of the distance from v to the target,
-     *  using Manhattan distance (or known as Taxicab metric) for calculation.
-     */
+    /** Estimate of the distance from v to the target. */
     private int h(int v) {
-        int sourceX = maze.toX(v);
-        int sourceY = maze.toY(v);
-        int targetX = maze.toX(t);
-        int targetY = maze.toY(t);
-        return Math.abs(sourceX - targetX) + Math.abs(sourceY - targetY);
+        return -1;
     }
 
     /** Finds vertex estimated to be closest to target. */
@@ -41,40 +31,7 @@ public class MazeAStarPath extends MazeExplorer {
 
     /** Performs an A star search from vertex s. */
     private void astar(int s) {
-        Comparator<Node> comparator = Comparator.comparingInt((Node n) -> n.priority);
-        PriorityQueue<Node> pq = new PriorityQueue<>(comparator);
-        HashMap<Integer, Node> map = new HashMap<>();
-        pq.add(new Node(s));
-
-        while (!pq.isEmpty()) {
-            int v = pq.remove().item;
-            marked[v] = true;
-            announce();
-
-            if (v == t) {
-                return;
-            }
-
-            for (int w : maze.adj(v)) {
-                if (!marked[w]) {
-                    int relaxed = distTo[v] + 1;
-                    if (relaxed < distTo[w]) {
-                        distTo[w] = relaxed;
-                        edgeTo[w] = v;
-                        pq.add(new Node(w));
-                    }
-                }
-            }
-        }
-    }
-
-    private class Node {
-        private int item;
-        private int priority;
-        Node(int v) {
-            this.item = v;
-            this.priority = distTo[v] + h(v);
-        }
+        // TODO
     }
 
     @Override
