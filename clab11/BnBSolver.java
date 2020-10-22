6c9dc49b151ef57b6fc13303dad47332e448a773
Alice Qian Huang <alice.huangqian97@gmail.com>
2020-10-22 02:54:54 -0500

    Work in progress

--- clab11/BnBSolver.java
+++ clab11/BnBSolver.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -10,8 +11,16 @@ import java.util.List;
  */
 public class BnBSolver {
 
+    private List<Bear> sortedBears;
+    private List<Bed> sortedBeds;
+    private Node root;
+
     public BnBSolver(List<Bear> bears, List<Bed> beds) {
         // TODO: Fix me.
+        root = new Node(beds.get(0));
+        buildLLRB(root, beds, bears.get(0), 1);
+        sortedBears = new ArrayList<>();
+        sortedBeds = new ArrayList<>();
     }
 
     /**
@@ -19,7 +28,7 @@ public class BnBSolver {
      */
     public List<Bear> solvedBears() {
         // TODO: Fix me.
-        return null;
+        return sortedBears;
     }
 
     /**
@@ -27,6 +36,48 @@ public class BnBSolver {
      */
     public List<Bed> solvedBeds() {
         // TODO: Fix me.
-        return null;
+        return sortedBeds;
+    }
+
+    /**
+     * Use one random bear as the comparator to build a LLRB of beds.
+     */
+    private void buildLLRB(Node curr, List<Bed> beds, Bear bearCeleb, int index) {
+        if (index == beds.size()) {
+            return;
+        }
+        if (curr null) {
+
+        }
+        int zero = curr.bed.compareTo(bearCeleb);
+        int val = beds.get(index).compareTo(bearCeleb);
+        if (val >= zero) {
+            buildLLRB(curr.right, beds, bearCeleb, index);
+        } else {
+            buildLLRB(curr.left, beds, bearCeleb, index);
+        }
+        int center = beds.get(0).compareTo(bear);
+        for (int i = 1; i < beds.size(); i++) {
+            if (beds.get(i).compareTo(bear) > center) {
+                center.
+            }
+        }
+    }
+
+    private void rotateLeft() {
+
+    }
+
+    private void rotateRight() {
+
+    }
+
+    private class Node {
+        private Bed bed;
+        private Node left;
+        private Node right;
+        Node(Bed bed) {
+            this.bed = bed;
+        }
     }
 }
