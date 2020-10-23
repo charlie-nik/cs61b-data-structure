dd6d2f58026f67a3bee56a6f6774482e06be8b45
Alice Qian Huang <alice.huangqian97@gmail.com>
2020-10-22 23:21:48 -0500

    Completed Pt.II & III (gold point) of Proj2C

--- proj2c/bearmaps/proj2c/AugmentedStreetMapGraph.java
+++ proj2c/bearmaps/proj2c/AugmentedStreetMapGraph.java
@@ -2,9 +2,13 @@ package bearmaps.proj2c;
 
 import bearmaps.hw4.streetmap.Node;
 import bearmaps.hw4.streetmap.StreetMapGraph;
+import bearmaps.proj2ab.KDTree;
 import bearmaps.proj2ab.Point;
+import bearmaps.proj2ab.PointSet;
+import edu.princeton.cs.algs4.TrieSET;
 
 import java.util.*;
+
 
 /**
  * An augmented graph that is more powerful that a standard StreetMapGraph.
@@ -15,10 +19,35 @@ import java.util.*;
  */
 public class AugmentedStreetMapGraph extends StreetMapGraph {
 
+    private PointSet kdTree;
+    private Map<Point, Node> pointToNode;
+    private TrieSET trie;
+    private Map<String, HashSet<Node>> nameToNode;
+
     public AugmentedStreetMapGraph(String dbPath) {
         super(dbPath);
-        // You might find it helpful to uncomment the line below:
-        // List<Node> nodes = this.getNodes();
+
+        List<Point> points = new LinkedList<>();
+        pointToNode = new HashMap<>();
+        nameToNode = new HashMap<>();
+        trie = new TrieSET();
+        for (Node n : this.getNodes()) {
+            if (neighbors(n.id()).size() > 0) {
+                Point p = new Point(n.lon(), n.lat());
+                points.add(p);
+                pointToNode.put(p, n);
+            }
+            if (n.name() != null) {
+                String cleanedName = cleanString(n.name());
+                trie.add(cleanedName);
+                if (!nameToNode.containsKey(cleanedName)) {
+                    nameToNode.put(cleanedName, new HashSet<>());
+                }
+                nameToNode.get(cleanedName).add(n);
+            }
+        }
+
+        kdTree = new KDTree(points);
     }
 
 
@@ -30,20 +59,29 @@ public class AugmentedStreetMapGraph extends StreetMapGraph {
      * @return The id of the node in the graph closest to the target.
      */
     public long closest(double lon, double lat) {
-        return 0;
+        Point p = kdTree.nearest(lon, lat);
+        return pointToNode.get(p).id();
     }
 
 
     /**
      * For Project Part III (gold points)
      * In linear time, collect all the names of OSM locations that prefix-match the query string.
-     * @param prefix Prefix string to be searched for. Could be any case, with our without
+     * Runtime should be O(k) where k is the number of words sharing the input prefix.
+     * @param prefix Prefix string to be searched for. Could be any case, with or without
      *               punctuation.
      * @return A <code>List</code> of the full names of locations whose cleaned name matches the
      * cleaned <code>prefix</code>.
      */
     public List<String> getLocationsByPrefix(String prefix) {
-        return new LinkedList<>();
+        List<String> locations = new LinkedList<>();
+        List<String> keys = (List<String>) trie.keysWithPrefix(prefix);
+        for (String key : keys) {
+            for (Node n : nameToNode.get(key)) {
+                locations.add(n.name());
+            }
+        }
+        return locations;
     }
 
     /**
@@ -60,7 +98,17 @@ public class AugmentedStreetMapGraph extends StreetMapGraph {
      * "id" -> Number, The id of the node. <br>
      */
     public List<Map<String, Object>> getLocations(String locationName) {
-        return new LinkedList<>();
+        List<Map<String, Object>> locations = new LinkedList<>();
+        Set<Node> nodes = nameToNode.get(cleanString(locationName));
+        for (Node n : nodes) {
+            Map<String, Object> info = new HashMap<>();
+            info.put("lat", n.lat());
+            info.put("lon", n.lon());
+            info.put("name", n.name());
+            info.put("id", n.id());
+            locations.add(info);
+        }
+        return locations;
     }
 
 
