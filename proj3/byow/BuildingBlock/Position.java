24a9f65f3a91e30431f1babcbebce73993b7f021
Alice Qian Huang <alice.huangqian97@gmail.com>
2020-10-29 00:29:48 -0500

    Changed Position to an independent non-static class hoping to make serialization work (and it doesn't)

---
+++ proj3/byow/BuildingBlock/Position.java
@@ -0,0 +1,53 @@
+package byow.BuildingBlock;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Position implements Serializable {
+    private int x;
+    private int y;
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+    public void setX(int newX) {
+        x = newX;
+    }
+    public void setY(int newY) {
+        y = newY;
+    }
+
+    public Position east() {
+        return new Position(x + 1, y);
+    }
+    public Position west() {
+        return new Position(x - 1, y);
+    }
+    public Position north() {
+        return new Position(x, y + 1);
+    }
+    public Position south() {
+        return new Position(x, y - 1);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return x == position.x &&
+                y == position.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+}
